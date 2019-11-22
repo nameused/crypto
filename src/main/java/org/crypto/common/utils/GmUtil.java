101e6460a9e70db787b2947c0298d5018f7bf7e7
linuxmint <zmy@dxct.org>
2019-11-22 14:57:22 +0800

    增加sm2符合国密标准的转换工具类

---
+++ src/main/java/org/crypto/common/utils/GmUtil.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright DingXuan. All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.crypto.common.utils;
+
+import org.bouncycastle.asn1.ASN1EncodableVector;
+import org.bouncycastle.asn1.ASN1Integer;
+import org.bouncycastle.asn1.ASN1Sequence;
+import org.bouncycastle.asn1.DERSequence;
+import org.bouncycastle.util.Arrays;
+import org.bouncycastle.util.encoders.Hex;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+/**
+ * @author zhangmingyang
+ * @Date: 11/22/19
+ * @Version 1.0.0
+ */
+public class GmUtil {
+    private final static int RS_LEN = 32;
+    public static byte[] bigIntToFixexLengthBytes(BigInteger rOrS) {
+        // for sm2p256v1, n is 00fffffffeffffffffffffffffffffffff7203df6b21c6052b53bbf40939d54123,
+        // r and s are the result of mod n, so they should be less than n and have length<=32
+        byte[] rs = rOrS.toByteArray();
+        if (rs.length == RS_LEN) return rs;
+        else if (rs.length == RS_LEN + 1 && rs[0] == 0) return Arrays.copyOfRange(rs, 1, RS_LEN + 1);
+        else if (rs.length < RS_LEN) {
+            byte[] result = new byte[RS_LEN];
+            Arrays.fill(result, (byte) 0);
+            System.arraycopy(rs, 0, result, RS_LEN - rs.length, rs.length);
+            return result;
+        } else {
+            throw new RuntimeException("err rs: " + Hex.toHexString(rs));
+        }
+    }
+
+    /**
+     * BC的SM3withSM2签名得到的结果的rs是asn1格式的，这个方法转化成直接拼接r||s
+     * @param rsDer rs in asn1 format
+     * @return sign result in plain byte array
+     */
+    public static byte[] rsAsn1ToPlainByteArray(byte[] rsDer) {
+        ASN1Sequence seq = ASN1Sequence.getInstance(rsDer);
+        byte[] r = bigIntToFixexLengthBytes(ASN1Integer.getInstance(seq.getObjectAt(0)).getValue());
+        byte[] s = bigIntToFixexLengthBytes(ASN1Integer.getInstance(seq.getObjectAt(1)).getValue());
+        byte[] result = new byte[RS_LEN * 2];
+        System.arraycopy(r, 0, result, 0, r.length);
+        System.arraycopy(s, 0, result, RS_LEN, s.length);
+        return result;
+    }
+
+    /**
+     * BC的SM3withSM2验签需要的rs是asn1格式的，这个方法将直接拼接r||s的字节数组转化成asn1格式
+     * @param sign in plain byte array
+     * @return rs result in asn1 format
+     */
+    public static byte[] rsPlainByteArrayToAsn1(byte[] sign){
+        if(sign.length != RS_LEN * 2) throw new RuntimeException("err rs. ");
+        BigInteger r = new BigInteger(1, Arrays.copyOfRange(sign, 0, RS_LEN));
+        BigInteger s = new BigInteger(1, Arrays.copyOfRange(sign, RS_LEN, RS_LEN * 2));
+        ASN1EncodableVector v = new ASN1EncodableVector();
+        v.add(new ASN1Integer(r));
+        v.add(new ASN1Integer(s));
+        try {
+            return new DERSequence(v).getEncoded("DER");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/test/java/org/crypto/algorithm/gm/sign/SM2Test.java
+++ src/test/java/org/crypto/algorithm/gm/sign/SM2Test.java
@@ -1,13 +1,19 @@
 package org.crypto.algorithm.gm.sign;
 
 import com.sun.org.apache.xml.internal.security.utils.Base64;
+import org.bouncycastle.asn1.*;
 import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPrivateKey;
 import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPublicKey;
+import org.bouncycastle.util.Arrays;
 import org.bouncycastle.util.encoders.Hex;
 import org.crypto.common.exception.SignException;
+import org.crypto.common.utils.GmUtil;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.math.BigInteger;
 import java.security.KeyPair;
 
 
@@ -36,18 +42,29 @@ public class SM2Test {
     public void sign() throws SignException {
 
         String data = "this is test data";
+        System.out.println("原文：" + Hex.toHexString(data.getBytes()));
         byte[] signature = sm2.sign(data.getBytes(), keyPair.getPrivate());
-        System.out.println(Hex.decode("c9e1496065d4dba61d27657d808c8affad321057e0c0f0bb37e4e4e153901e3e022021d1419c525e8b189ad7464dfe04bb44be6dbfb06e420dbc0a23958c914bee8e").length);
-        System.out.println("签名值："+Hex.toHexString(signature));
-        System.out.println("签名长度："+signature.length*8);
-        BCECPrivateKey bcecPrivateKey = (BCECPrivateKey) keyPair.getPrivate();
-        System.out.println("私钥长度："+bcecPrivateKey.getD().toByteArray().length);
-        System.out.println("私钥内容："+ Hex.toHexString(bcecPrivateKey.getD().toByteArray()));
 
+        System.out.println("BC实现的签名值：" + Hex.toHexString(signature));
+        System.out.println("签名长度：" + signature.length * 8);
+
+        //BC　实现的签名值为rs的asn1格式，需要转换为r||s拼接的方式，才符合国密检测工具的验证
+        byte[] encode = GmUtil.rsAsn1ToPlainByteArray(signature);
+        System.out.println("符合国密标准的签名值:" + Hex.toHexString(encode));
+        System.out.println("签名长度：" + encode.length * 8);
+
+        //r||s的原文转换为asn1格式
+        byte[] decode=GmUtil.rsPlainByteArrayToAsn1(encode);
+
+        Assert.assertArrayEquals(signature,decode);
+
+        BCECPrivateKey bcecPrivateKey = (BCECPrivateKey) keyPair.getPrivate();
+        System.out.println("私钥长度：" + bcecPrivateKey.getD().toByteArray().length);
+        System.out.println("私钥内容：" + Hex.toHexString(bcecPrivateKey.getD().toByteArray()));
 
         BCECPublicKey bcecPublicKey = (BCECPublicKey) keyPair.getPublic();
-        System.out.println("公钥长度："+bcecPublicKey.getQ().getEncoded(false).length);
-        System.out.println("公钥内容："+Hex.toHexString(bcecPublicKey.getQ().getEncoded(false)));
+        System.out.println("公钥长度：" + bcecPublicKey.getQ().getEncoded(false).length);
+        System.out.println("公钥内容：" + Hex.toHexString(bcecPublicKey.getQ().getEncoded(false)));
 
         boolean result = sm2.verify(data.getBytes(), keyPair.getPublic(), signature);
         System.out.println("verify result:" + result);

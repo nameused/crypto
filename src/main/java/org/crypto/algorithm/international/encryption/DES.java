f09297ffb085c845825b60c348b5aa75ad7c8b98
panshicheng <psc@dxct.org>
2019-12-01 18:40:07 +0800

    增加加密接口与DES实现

--- src/main/java/org/crypto/algorithm/international/encryption/DES.java
+++ src/main/java/org/crypto/algorithm/international/encryption/DES.java
@@ -16,10 +16,49 @@
 
 package org.crypto.algorithm.international.encryption;
 
+import org.crypto.common.exception.EncryptException;
+import org.crypto.intfs.IEncrypt;
+
+import javax.crypto.*;
+import javax.crypto.spec.DESKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+
 /**
  * @Author: zhangmingyang
  * @Date: 2019/11/19
  * @Company Dingxuan
  */
-public class DES {
+public class DES implements IEncrypt {
+    @Override
+    public byte[] enprypt(byte[] key, byte[] originalText, String encryptMode) throws EncryptException {
+        SecureRandom secureRandom = new SecureRandom();
+        // 从原始密钥数据创建DESKeySpec对象
+        DESKeySpec dks = null;
+        Cipher cipher = null;
+        byte[] encryptData = null;
+        try {
+            dks = new DESKeySpec(key);
+            // 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
+            SecretKeyFactory keyFactory = null;
+            keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKey securekey = null;
+            securekey = keyFactory.generateSecret(dks);
+            // Cipher对象实际完成加密操作
+            cipher = Cipher.getInstance("DES");
+            cipher.init(Cipher.ENCRYPT_MODE, securekey, secureRandom);
+            encryptData = cipher.doFinal(originalText);
+        } catch (InvalidKeyException | InvalidKeySpecException | NoSuchAlgorithmException
+                | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException e) {
+            e.printStackTrace();
+        }
+        return encryptData;
+    }
+
+    @Override
+    public byte[] decrypt(byte[] key, byte[] encryptText, String encryptMode) throws EncryptException {
+        return new byte[0];
+    }
 }
--- src/main/java/org/crypto/intfs/IEncrypt.java
+++ src/main/java/org/crypto/intfs/IEncrypt.java
@@ -1,11 +1,29 @@
 package org.crypto.intfs;
 
+import org.crypto.common.exception.EncryptException;
+
 /**
  * @Author: zhangmingyang
  * @Date: 2019/11/19
  * @Company Dingxuan
  */
 public interface IEncrypt {
-    byte[] enprypt();
+    /**
+     * 数据加密
+     *
+     * @param key
+     * @param originalText
+     * @return
+     */
+    byte[] enprypt(byte[] key, byte[] originalText, String encryptMode) throws EncryptException;
 
+    /**
+     * 数据解密
+     *
+     * @param key
+     * @param encryptText
+     * @param encryptMode
+     * @return
+     */
+    byte[] decrypt(byte[] key, byte[] encryptText, String encryptMode) throws EncryptException;
 }

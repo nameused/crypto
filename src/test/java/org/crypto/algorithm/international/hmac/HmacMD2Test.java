41db3d6a9d71127f58af12c45d8906c03f6b3624
zmy <zmy@dxct.org>
2019-12-08 21:59:19 +0800

    增加hmac算法实现和测试类

---
+++ src/test/java/org/crypto/algorithm/international/hmac/HmacMD2Test.java
@@ -0,0 +1,37 @@
+package org.crypto.algorithm.international.hmac;
+
+import org.bouncycastle.util.encoders.Hex;
+import org.crypto.common.exception.HmacException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Author: zhangmingyang
+ * @Date: 2019/12/8
+ * @Company Dingxuan
+ */
+public class HmacMD2Test {
+
+    HmacMD2 hmacMD2;
+    byte[] key;
+    @Before
+    public void setup() throws HmacException {
+        hmacMD2=new HmacMD2();
+        key=hmacMD2.initKey();
+    }
+
+    @Test
+    public void initKey() {
+    }
+
+    @Test
+    public void hmac() throws HmacException { String testData = "test message";
+        byte[] hash = hmacMD2.hmac(testData.getBytes(),key);
+        Assert.assertNotNull(hash);
+        System.out.println("digest length:" + hash.length * 8);
+        System.out.println("test message‘s hash value:" + Hex.toHexString(hash));
+    }
+}

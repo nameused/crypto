76bb4e1b14f48a2131111226859360ac4e456ad7
zhangmingyang <zmy@dxct.org>
2019-12-31 17:47:15 +0800

    增加RC2、RC5和IDEA算法实现和测试类

--- src/main/java/org/crypto/algorithm/international/encryption/RC2.java
+++ src/main/java/org/crypto/algorithm/international/encryption/RC2.java
@@ -1,5 +1,5 @@
 /**
- * Copyright Dingxuan. All Rights Reserved.
+ * Copyright DingXuan. All Rights Reserved.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,10 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.crypto.algorithm.international.encryption;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.crypto.common.exception.EncryptException;
 import org.crypto.common.log.CryptoLog;
 import org.crypto.common.log.CryptoLogFactory;
@@ -25,29 +25,22 @@ import org.crypto.intfs.IEncrypt;
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
 
 /**
- * AES 加密算法实现
- * java 6支持的密钥长度为 128、192、256默认128
- * 若使用256位密钥需要获得无政策限制权限
- * 填充方式支持 NoPadding、PKCS5Padding、ISO10126Padding、
- * ANSIX923Padding
- * <p>
- * BC 支持密钥长度同上、填充方式支持PKCS7Padding、ZeroBytePadding
- *
- * @Author: zhangmingyang
- * @Date: 2019/12/24
- * @Company Dingxuan
+ * RC2算法实现
+ * 密钥可变换，支持从1字节到128字节
+ * 常用为8字节
+ * 由BC实现，支持填充类型包含
+ * PKCS5Padding、PKCS7Padding、ISO10126Padding
+ * @author zhangmingyang
+ * @Date: 2019/12/31
+ * @company Dingxuan
  */
-public class AES implements IEncrypt {
+public class RC2 implements IEncrypt {
+    private static CryptoLog log = CryptoLogFactory.getLog(RC2.class);
 
-    private static CryptoLog log = CryptoLogFactory.getLog(AES.class);
-
-    private static final String KEY_ALGORITHM = "AES";
+    private static final String KEY_ALGORITHM = "RC2";
 
     /**
      * 密钥转换
@@ -57,7 +50,7 @@ public class AES implements IEncrypt {
      * @throws EncryptException
      */
     private static Key convertKey(byte[] key, String keyAlgorithm) throws EncryptException {
-        SecretKey secretKey = new SecretKeySpec(key,keyAlgorithm);
+        SecretKey secretKey = new SecretKeySpec(key, keyAlgorithm);
         return secretKey;
     }
 
@@ -72,6 +65,7 @@ public class AES implements IEncrypt {
     public byte[] genKey(int keyLength) throws EncryptException {
         KeyGenerator keyGenerator = null;
         try {
+            Security.addProvider(new BouncyCastleProvider());
             keyGenerator = KeyGenerator.getInstance(KEY_ALGORITHM);
         } catch (NoSuchAlgorithmException e) {
             log.error(e.getMessage());
@@ -84,6 +78,7 @@ public class AES implements IEncrypt {
 
     @Override
     public byte[] encrypt(String cipherAlgorithm, byte[] key, byte[] iv, byte[] originalText) throws EncryptException {
+        Security.addProvider(new BouncyCastleProvider());
         Key secretKey = convertKey(key, KEY_ALGORITHM);
         byte[] encryptData = null;
         try {
@@ -105,6 +100,7 @@ public class AES implements IEncrypt {
 
     @Override
     public byte[] decrypt(String cipherAlgorithm, byte[] key, byte[] iv, byte[] encryptText) throws EncryptException {
+        Security.addProvider(new BouncyCastleProvider());
         Key secretKey = convertKey(key, KEY_ALGORITHM);
         byte[] decryptData = null;
         Cipher cipher = null;
--- src/main/java/org/crypto/algorithm/international/encryption/RC5.java
+++ src/main/java/org/crypto/algorithm/international/encryption/RC5.java
@@ -1,5 +1,5 @@
 /**
- * Copyright Dingxuan. All Rights Reserved.
+ * Copyright DingXuan. All Rights Reserved.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,10 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.crypto.algorithm.international.encryption;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.crypto.common.exception.EncryptException;
 import org.crypto.common.log.CryptoLog;
 import org.crypto.common.log.CryptoLogFactory;
@@ -25,29 +25,17 @@ import org.crypto.intfs.IEncrypt;
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
 
 /**
- * AES 加密算法实现
- * java 6支持的密钥长度为 128、192、256默认128
- * 若使用256位密钥需要获得无政策限制权限
- * 填充方式支持 NoPadding、PKCS5Padding、ISO10126Padding、
- * ANSIX923Padding
- * <p>
- * BC 支持密钥长度同上、填充方式支持PKCS7Padding、ZeroBytePadding
- *
- * @Author: zhangmingyang
- * @Date: 2019/12/24
- * @Company Dingxuan
+ * @author zhangmingyang
+ * @Date: 2019/12/31
+ * @company Dingxuan
  */
-public class AES implements IEncrypt {
+public class RC5 implements IEncrypt {
+    private static CryptoLog log = CryptoLogFactory.getLog(RC5.class);
 
-    private static CryptoLog log = CryptoLogFactory.getLog(AES.class);
-
-    private static final String KEY_ALGORITHM = "AES";
+    private static final String KEY_ALGORITHM = "RC5";
 
     /**
      * 密钥转换
@@ -57,7 +45,7 @@ public class AES implements IEncrypt {
      * @throws EncryptException
      */
     private static Key convertKey(byte[] key, String keyAlgorithm) throws EncryptException {
-        SecretKey secretKey = new SecretKeySpec(key,keyAlgorithm);
+        SecretKey secretKey = new SecretKeySpec(key, keyAlgorithm);
         return secretKey;
     }
 
@@ -72,6 +60,7 @@ public class AES implements IEncrypt {
     public byte[] genKey(int keyLength) throws EncryptException {
         KeyGenerator keyGenerator = null;
         try {
+            Security.addProvider(new BouncyCastleProvider());
             keyGenerator = KeyGenerator.getInstance(KEY_ALGORITHM);
         } catch (NoSuchAlgorithmException e) {
             log.error(e.getMessage());
@@ -84,6 +73,7 @@ public class AES implements IEncrypt {
 
     @Override
     public byte[] encrypt(String cipherAlgorithm, byte[] key, byte[] iv, byte[] originalText) throws EncryptException {
+        Security.addProvider(new BouncyCastleProvider());
         Key secretKey = convertKey(key, KEY_ALGORITHM);
         byte[] encryptData = null;
         try {
@@ -105,6 +95,7 @@ public class AES implements IEncrypt {
 
     @Override
     public byte[] decrypt(String cipherAlgorithm, byte[] key, byte[] iv, byte[] encryptText) throws EncryptException {
+        Security.addProvider(new BouncyCastleProvider());
         Key secretKey = convertKey(key, KEY_ALGORITHM);
         byte[] decryptData = null;
         Cipher cipher = null;
